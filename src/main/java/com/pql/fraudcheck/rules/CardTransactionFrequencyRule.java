4ed5d67697c77c8e1b1b5af4e620a1689b4480d1
Pasquale Ricupero <pasquale_ricupero@mastercard.com>
2021-05-07 19:35:56 +0100

    class name refactor

---
+++ src/main/java/com/pql/fraudcheck/rules/CardTransactionFrequencyRule.java
@@ -0,0 +1,40 @@
+package com.pql.fraudcheck.rules;
+
+import com.pql.fraudcheck.dto.FraudRuleScore;
+import com.pql.fraudcheck.dto.IncomingTransactionInfo;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by pasqualericupero on 06/05/2021.
+ */
+@Log4j2
+@Component("CARD_FREQUENCY")
+public class CardTransactionFrequencyRule implements IFraudDetection {
+
+    @Override
+    public FraudRuleScore checkFraud(IncomingTransactionInfo transInfo) {
+        log.info("Processing card recent transaction number::{}", transInfo.getRecentCardTransactionNumber());
+
+        Integer fraudScore;
+        Integer transFrequency = transInfo.getRecentCardTransactionNumber();
+        String message = "";
+
+        if (transFrequency <= 25) {
+            fraudScore = 0;
+        } else if (transFrequency <= 50) {
+            fraudScore = 15;
+        } else if (transFrequency <= 100) {
+            fraudScore = 50;
+        } else {
+            fraudScore = 80;
+        }
+
+        if (fraudScore > 0) {
+            message = "Card transaction frequency suspicious";
+            log.warn("{}::{}", message, transFrequency);
+        }
+
+        return new FraudRuleScore(fraudScore, message);
+    }
+}
