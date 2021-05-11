2fe5917ed30c0fe236835e3d71c7902ade14f503
Pasquale Ricupero <pasquale_ricupero@mastercard.com>
2021-05-11 17:43:56 +0100

    just a comment/reminder

--- src/main/java/com/pql/fraudcheck/service/FraudCheckService.java
+++ src/main/java/com/pql/fraudcheck/service/FraudCheckService.java
@@ -51,7 +51,7 @@ public class FraudCheckService {
     public FraudCheckResponse checkAllFraudRules(FraudCheckRequest request) {
         log.info("Started fraud check for cardNumber::{} and terminalId::{}", CardUtil.getMaskedPan(request.getCardNumber()), request.getTerminalId());
 
-        // if currency is invalid there is no need to proceed
+        // why this check here? if currency is invalid then there is no need to proceed, return 422 and save time
         checkCurrency(request.getCurrency());
 
         FraudCheckResponse response;
